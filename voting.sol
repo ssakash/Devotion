ea5ed117eaa9b1ec1eeff787df2d33a4ea0d40b1
Akash <ssj2akash@gmail.com>
2024-07-24 15:55:49 +0530

    Add decentralized voting

---
+++ voting.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract voting {
+
+   address owner;
+
+   constructor ()
+   {
+    owner = msg.sender;
+   }
+
+  struct vote {
+    address walletaddr;
+    string firstname;
+    string lastname;
+    uint votes;
+  }
+
+  vote[] public candidates;
+
+  function addcandidate(address walletaddr, string memory firstname, string memory lastname, uint votes) public
+    {
+        candidates.push(vote(walletaddr,firstname,lastname, votes));
+    }
+
+  function addvotetocandidate(address walletaddr) private 
+  {
+       for(uint i=0; i < candidates.length;i++)
+        {
+            if(candidates[i].walletaddr == walletaddr)
+                candidates[i].votes += 1;
+        }
+  }
+  function voteforcandidate (address walletaddr) payable public {addvotetocandidate(walletaddr);}
+
+}
